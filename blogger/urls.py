774553704de7655c0a7377fe12047e7335d02bb4
Sourabh <52883312+Sourabh509511@users.noreply.github.com>
2020-09-11 15:42:06 +0530

    upgarde

--- blogger/urls.py
+++ blogger/urls.py
@@ -9,6 +9,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name="home"),
-    path('blogger/<str:slug>', views.blogger,name="blogger"),
-    path('comment', views.postcomment,name="comment"),
+    path('blogger/<str:slug>', views.blogger.as_view(),name="blogger"),
+    path('comment', views.postcomment.as_view(),name="comment"),
 ]
